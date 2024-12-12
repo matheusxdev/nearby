6a2be7beaaba08262786e1d753aa72aa27b3967a
Matheus Oliveira <matheusxdeveloper@gmail.com>
2024-12-12 05:16:24 -0300

    atualizacoes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,36 @@
-import {Stack } from "expo-router";
+import { Stack } from "expo-router"
 import { colors } from "@/styles/theme"
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from "@expo-google-fonts/rubik"
-import  { Loading } from "@/components/loading"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
-export default function Layout(){
-  const [fontsLoaded] = useFonts({ Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold, })
+import {
+  useFonts,
+  Rubik_600SemiBold,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik"
 
-  if(!fontsLoaded){
+import { Loading } from "@/components/loading"
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+  })
+
+  if (!fontsLoaded) {
     return <Loading />
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack 
-        screenOptions={{ 
+      <Stack
+        screenOptions={{
           headerShown: false,
           contentStyle: { backgroundColor: colors.gray[100] },
-        }} 
+        }}
       />
     </GestureHandlerRootView>
   )
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,37 +2,31 @@ import { useEffect, useState } from "react"
 import { View, Alert, Text } from "react-native"
 import MapView, { Callout, Marker } from "react-native-maps"
 import * as Location from "expo-location"
-import { api } from "@/services/api"
-import { colors, fontFamily } from "@/styles/theme"
-import { Categories, CategoriesProps } from "@/components/categories"
-import { PlaceProps } from "@/components/place"
-import { Places } from "@/components/places"
+import { router } from "expo-router"
 
-type MarketProps = PlaceProps & {
-  latitude: number,
+import { api } from "@/services/api"
+import { fontFamily, colors } from "@/styles/theme"
+
+import { Places } from "@/components/places"
+import { PlaceProps } from "@/components/place"
+import { Categories, CategoriesProps } from "@/components/categories"
+
+type MarketsProps = PlaceProps & {
+  latitude: number
   longitude: number
 }
 
 const currentLocation = {
   latitude: -23.561187293883442,
   longitude: -46.656451388116494,
-  latitudeDelta: 0.01, 
-  longitudeDelta: 0.01
 }
 
-export default function Home(){
+export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([])
   const [category, setCategory] = useState("")
-  const [markets, setMarkets] = useState<MarketProps[]>([])
+  const [markets, setMarkets] = useState<MarketsProps[]>([])
 
-  const [regiao, setRegiao] = useState({
-    latitude: -23.561187293883442,
-    longitude: -46.656451388116494,
-    latitudeDelta: 0.01, 
-    longitudeDelta: 0.01
-  })
-
-  async function fetchCategories(){
+  async function fetchCategories() {
     try {
       const { data } = await api.get("/categories")
       setCategories(data)
@@ -43,35 +37,27 @@ export default function Home(){
     }
   }
 
-  async function fetchMarkets(){
+  async function fetchMarkets() {
     try {
-      if(!category){
+      if (!category) {
         return
       }
 
-      const {data} = await api.get("/markets/category/" + category)
+      const { data } = await api.get("/markets/category/" + category)
       setMarkets(data)
     } catch (error) {
       console.log(error)
-      Alert.alert("Locais", "Não foi possível carregar os locais")
+      Alert.alert("Locais", "Não foi possível carregar os locais.")
     }
   }
 
-  async function getCurrentLocation(){
+  async function getCurrentLocation() {
     try {
       const { granted } = await Location.requestForegroundPermissionsAsync()
 
-      if(granted){
-        let meulocal = await Location.getCurrentPositionAsync()
-        const { latitude, longitude } = meulocal.coords
-        //console.log("latitude:" + latitude, "longitude:" + longitude)
-
-        setRegiao({
-          latitude: meulocal.coords.latitude,
-          longitude: meulocal.coords.longitude,
-          latitudeDelta: 0.1, 
-          longitudeDelta: 0.1
-        })
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync()
+        console.log(location)
       }
     } catch (error) {
       console.log(error)
@@ -79,9 +65,8 @@ export default function Home(){
   }
 
   useEffect(() => {
-    getCurrentLocation()
     fetchCategories()
-  }, [regiao])
+  }, [])
 
   useEffect(() => {
     fetchMarkets()
@@ -89,35 +74,68 @@ export default function Home(){
 
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
-      <Categories data={categories} onSelect={setCategory} selected={category} />
+      <Categories
+        data={categories}
+        onSelect={setCategory}
+        selected={category}
+      />
 
-      <MapView style={{ flex: 1 }} initialRegion={{ latitude: currentLocation.latitude, longitude: currentLocation.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }} region={{ latitude: currentLocation.latitude, longitude: currentLocation.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }}>
-        <Marker identifier="current" coordinate={{ latitude: regiao.latitude, longitude: regiao.longitude }} image={require("@/assets/location.png")} />
-      </MapView>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
 
-      {markets.map(( item ) => (
-          <Marker 
+        {markets.map((item) => (
+          <Marker
             key={item.id}
             identifier={item.id}
-            coordinate={{ latitude: item.latitude, longitude: item.longitude }}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
             image={require("@/assets/pin.png")}
           >
-            <Callout>
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
               <View>
-                <Text style={{ fontSize: 14, color: colors.gray[600], fontFamily: fontFamily.medium  }}>{item.name}</Text>
-                <Text style={{ fontSize: 12, color: colors.gray[600], fontFamily: fontFamily.regular  }}>{item.address}</Text>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {item.name}
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {item.address}
+                </Text>
               </View>
             </Callout>
           </Marker>
-        ))
-      }
+        ))}
+      </MapView>
 
       <Places data={markets} />
     </View>
   )
 }
-/*
-<MapView style={{ flex: 1 }} initialRegion={regiao} region={regiao}>
-  <Marker identifier="current" coordinate={{ latitude: regiao.latitude, longitude: regiao.longitude }} image={require("@/assets/location.png")} />
-</MapView>
-*/
